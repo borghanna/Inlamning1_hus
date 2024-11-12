2c4b6dd9ef7ac2e41f351870036c8a03fc325511
borghanna <hanna.borg@medieinstitutet.se>
2024-11-12 14:46:01 +0100

    styling + firstscene

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 window.addEventListener("DOMContentLoaded", main);
+
+function main() {
+  loadingWelcomeScene();
+}
+function loadingWelcomeScene() {
+  topButton.onclick = loadingFirstScene;
+  // leftButton.className = "btn hidden";
+  leftButton.classList.add("hidden"); //remove när de ska visas
+  rightButton.classList.add("hidden"); //remove när de ska visas
+  bottomButton.classList.add("hidden");
+}
+
+function loadingFirstScene() {
+  heading.innerHTML = "Hall 1";
+  const firstSceneP = document.getElementById("mainText");
+  firstSceneP.innerHTML = "Hej test hej";
+  leftButton.classList.remove("hidden");
+  rightButton.classList.remove("hidden");
+  bottomButton.classList.remove("hidden");
+}
